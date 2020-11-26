ffbfffd43f0dcd330797c333e36dd05c86033859
Pirmin <pzd3mir@googlemail.com>
2020-11-26 19:53:43 +0100

    Added Tax rate to be set by government only

--- contracts/realEstate.sol
+++ contracts/realEstate.sol
@@ -1,7 +1,40 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.7.0;
 
-contract realEstate {
-    function realEstate(){
+contract realEstate{
+    /// Declare state variables in this section
+  ///  address public owner = msg.sender;
+    address public gov = msg.sender;
+    uint8 public tax = 100;
 
+    /// Define events
+
+    /// Define modifiers in this section
+    modifier onlyGov{
+        require(msg.sender == owner);
+        _;
+}
+    modifier onlyPropOwner{
+        require(msg.sender == owner);
+        _;
+}
+    /// Define functions in this section
+
+  ///  function payRent () payable {
+///    deposits[msg.sender] += msg.value;
+///}
+
+    function setTax (uint8 _x) public onlyGov {
+        require( _x <= 100, "Valid tax rate  (0% - 100% required)" );
+        {
+            tax = _x;
+        }
     }
+
+
+    receive() external payable {
+
+}
+
+   /// function realEstate(){
+    ///}
 }
