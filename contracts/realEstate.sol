b04956625e1526651d7effd1206649a776a20e8f
Pirmin <pzd3mir@googlemail.com>
2020-11-26 23:13:53 +0100

    -Added Constructor function.
    Can now input @ contract creation:
    
     _initialPropertyShares [Number of existing property shares]
     _propertyID [The Token Name]
     _decimalUnits [should be 0 unless wanted differently]
     _propertySymbol
     _mainPropertyOwner [gets all token on contract creation]
     _tax [sets tax rate]
    
     -Added ERC-20 function.
     Creates Contract with tradable coins.
    
     -function claimOwnership added. You can change the mainPropertyOwner if you are not the mainOwner and have more than 50% of the tokens.

--- contracts/realEstate.sol
+++ contracts/realEstate.sol
@@ -6,7 +6,7 @@ contract realEstate{
 
     address public gov = msg.sender;
     uint8 public tax;
-    address public mainPropertyOwner;
+     address public mainPropertyOwner;
 
     // Create ERC 20 necessary values
 
@@ -20,14 +20,27 @@ uint8 public decimals;
 string public symbol;
 
 
-    // Define events
+    // Define ERC 20 necessary events
+
+    event Transfer(
+address indexed from,
+address indexed to,
+uint256 value
+);
+event Approval(
+address indexed owner,
+address indexed spender,
+uint256 value
+);
+
     constructor (
-uint256 _initialPropertyShares,
+
 string memory _propertyID,
 uint8 _decimalUnits,
 string memory _propertySymbol,
 address _mainPropertyOwner,
-uint8 _tax
+uint8 _tax,
+uint256 _initialPropertyShares
 ) {
 balances[_mainPropertyOwner] = _initialPropertyShares;
 totalSupply = _initialPropertyShares;
@@ -53,12 +66,49 @@ mainPropertyOwner = _mainPropertyOwner;
 //    deposits[msg.sender] += msg.value;
 //}
 
+    function transfer(address _to, uint256 _value) public returns (bool success) {
+require(balances[msg.sender] >= _value);
+balances[msg.sender] -= _value;
+balances[_to] += _value;
+emit Transfer(msg.sender, _to, _value);
+return true;
+}
+function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
+uint256 allowance = allowed[_from][msg.sender];
+require(balances[_from] >= _value && allowance >= _value);
+balances[_to] += _value;
+balances[_from] -= _value;
+if (allowance < MAX_UINT256) {
+allowed[_from][msg.sender] -= _value;
+}
+emit Transfer(_from, _to, _value);
+return true;
+}
+function balanceOf(address _owner) public view returns (uint256 balance) {
+return balances[_owner];
+}
+function approve(address _spender, uint256 _value) public returns (bool success) {
+allowed[msg.sender][_spender] = _value;
+emit Approval(msg.sender, _spender, _value);
+return true;
+}
+function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+return allowed[_owner][_spender];
+}
+
+
     function setTax (uint8 _x) public onlyGov {
-        require( _x <= 100, "Valid tax rate  (0% - 100% required)" );
+        require( _x <= 100, "Valid tax rate  (0% - 100%) required" );
         {
             tax = _x;
         }
     }
+
+    function claimOwnership () public {
+        require(balances[msg.sender] > (totalSupply /2) && msg.sender != mainPropertyOwner,
+        "Error. You do not own more than 50% of the property tokens or you are the main owner allready");
+        mainPropertyOwner = msg.sender;
+  }
 
 
     receive() external payable {
