d03327f574285c4e3eca8597c0ef1f531ccafbfc
Pirmin <pzd3mir@googlemail.com>
2020-12-02 20:32:53 +0100

    receive function created gas problems (2300 gas restriction apparently?).
    New Function payRent created. automatically deducts taxes, automatically maps block untill rent is paid to address.
    
    Works so far, as long as gov and mainpropertyowner are not the same.

--- contracts/realEstate.sol
+++ contracts/realEstate.sol
@@ -8,6 +8,8 @@ contract realEstate {
     uint8 public avgBlockTime;                          // Avg block time in seconds. can be set by onlyGov
     uint8 public decimals;                              // Decimals of our House shares. Is set to 0 by default.
 	uint8 public tax;                               	// Can Preset Tax rate in constructor. To be changed by government only.
+	uint8 public rentalLimit;
+	uint256 public rentalLimitBlocks;
 	uint256 constant private MAX_UINT256 = 2**256 - 1;  // Very large number. Used here for gov having max allowance for stakeholder tokens to seize tokens if wanted.
 	uint256 public totalSupply;                         // By default we have 100 for 100%. Maybe possible to change later.
 	uint256 public totalSupply2;                   // Only Ether incoming of multiples of this variable will be allowed. This way we can have two itterations of divisions through totalSupply without remainder. There is no Float in ETH, so we need to prevent remainders in division. E.g. 1. iteration (incoming ether value = MultipleOfTokenSupplyPower2) / totalSupply * uint (desired percentage); 2nd iteration ( ether value = MultipleOfTokenSupplyPower) / totalSupply * uint (desired percentage); --> no remainder
@@ -17,6 +19,7 @@ contract realEstate {
 	uint256 public rentPaidUntillBlockNr;               // Rent is paid Untill this Blocknumber.
 	uint256 public _taxdeduct;                                 // ammount of tax to be paid for incoming ether.
 	uint256 public msgvalue;
+
 
 	string public name;                                 // The name of our house (token). Can be determined in Constructor _propertyID
 	string public symbol;                               // The Symbol of our house (token). Can be determined in Constructor _propertySymbol
@@ -28,8 +31,9 @@ contract realEstate {
 	address[] public stakeholders;                      // Array of stakeholders. Government can addStakeholder or removeStakeholder. Recipient of token needs to be isStakeholder = true to be able to receive token. mainPropertyOwner & Government are stakeholder by default.
 
 	mapping(address => uint256) public revenues;        // Distributed revenue account ballance for each stakeholder including gov.
-	mapping (address => uint256) public balances;       // Addresses mapped to token ballances.
+	mapping (address => uint256) public shares;       // Addresses mapped to token ballances.
 	mapping (address => mapping (address => uint256)) public allowed;   //Owner Addresses mapping eligible spenders addresses with value spenders are allowed to withdraw tokens from.
+	mapping (address => uint256) public rentpaidUntill;
 
 
 
@@ -39,7 +43,7 @@ contract realEstate {
 	event Approval(address indexed owner, address indexed spender, uint256 value);
 
 	constructor (string memory _propertyID, string memory _propertySymbol, address _mainPropertyOwner, uint8 _tax) {
-		balances[_mainPropertyOwner] = 100;
+		shares[_mainPropertyOwner] = 100;
 		totalSupply = 100;
 		totalSupply2 = totalSupply**2;
 		name = _propertyID;
@@ -52,6 +56,8 @@ contract realEstate {
         allowed[mainPropertyOwner][gov] = MAX_UINT256;
         avgBlockTime = 13;
 	    blocksPer30Day = 199385;
+	    rentalLimit = 12;
+	    rentalLimitBlocks = rentalLimit * blocksPer30Day;
 	}
 
 	// Define modifiers in this section
@@ -78,9 +84,9 @@ contract realEstate {
 
 	function transferFrom(address _from, address _to, uint256 _value) public returns (bool success) {
 		uint256 allowance = allowed[_from][msg.sender];
-		require(balances[_from] >= _value && allowance >= _value);
-		balances[_to] += _value;
-		balances[_from] -= _value;
+		require(shares[_from] >= _value && allowance >= _value);
+		shares[_to] += _value;
+		shares[_from] -= _value;
 		if (allowance < MAX_UINT256) {
 			allowed[_from][msg.sender] -= _value;
 		}
@@ -88,8 +94,8 @@ contract realEstate {
 		return true;
 	}
 
-	function balanceOf(address _owner) public view returns (uint256 balance) {
-		return balances[_owner];
+	function sharesOf(address _owner) public view returns (uint256 balance) {
+		return shares[_owner];
 	}
 
 	function approve(address _spender, uint256 _value) public returns (bool success) {
@@ -110,14 +116,14 @@ contract realEstate {
 	}
 
 	function claimOwnership () public {
-		require(balances[msg.sender] > (totalSupply /2) && msg.sender != mainPropertyOwner,"Error. You do not own more than 50% of the property tokens or you are the main owner allready");
+		require(shares[msg.sender] > (totalSupply /2) && msg.sender != mainPropertyOwner,"Error. You do not own more than 50% of the property tokens or you are the main owner allready");
 		mainPropertyOwner = msg.sender;
 	}
 
 	function _transfer(address _to, uint256 _value) private returns (bool success) {
-		require(balances[msg.sender] >= _value);
-	    balances[msg.sender] -= _value;
-	    balances[_to] += _value;
+		require(shares[msg.sender] >= _value);
+	    shares[msg.sender] -= _value;
+	    shares[_to] += _value;
 	    emit Transfer(msg.sender, _to, _value);
 		return true;
 	}
@@ -145,14 +151,18 @@ contract realEstate {
 	function assignTenant(address _tennant) public onlyPropOwner{
 	     tennant = _tennant;
 	}
+	function limitadvancedrent(uint8 _monthstolimit) public{
+	    rentalLimitBlocks = _monthstolimit *blocksPer30Day;
+	}
 
 	function SetAvgBlockTime (uint8 _sPerBlock) public onlyGov{
 	      require(_sPerBlock > 0, "Please enter a Value above 0");
 	      avgBlockTime = _sPerBlock;
 	      blocksPer30Day = (60*60*24*30) / avgBlockTime;
 	}
-    function setRentper30Day(uint8 _rent) public onlyPropOwner{
+    function setRentper30Day(uint256 _rent) public onlyPropOwner{
 	     rentPer30Day = _rent;
+
     }
 
 	function removeStakeholder(address _stakeholder) public onlyGov {
@@ -167,8 +177,8 @@ contract realEstate {
         uint256 _accumulated = accumulated;
         for (uint256 s = 0; s < stakeholders.length; s += 1){
             address stakeholder = stakeholders[s];
-            uint256 revenue = balanceOf(stakeholder);
-            uint256 ethertoreceive = (_accumulated/(totalSupply))*revenue;
+            uint256 _shares = sharesOf(stakeholder);
+            uint256 ethertoreceive = (_accumulated/(totalSupply))*_shares;
             accumulated = accumulated - ethertoreceive;
             revenues[stakeholder] = revenues[stakeholder] + ethertoreceive;
        }
@@ -187,12 +197,29 @@ contract realEstate {
 
     */
 
-    receive () external payable isMultipleOf onlyTennant{
-
-          /*   _taxdeduct = (msg.value/totalSupply * tax);
+    function payRent(uint8 _months) public payable isMultipleOf onlyTennant{
+            uint256  _rentdue  = _months * rentPer30Day;
+            uint256  _additionalblocks  = _months * blocksPer30Day;
+            require (msg.value == _rentdue && block.number + _additionalblocks < block.number + rentalLimitBlocks);
+            _taxdeduct = (msg.value/totalSupply * tax);
             accumulated += (msg.value - _taxdeduct);
             revenues[gov] += _taxdeduct;
-    	    rentPaidUntillBlockNr = block.number + (rentPer30Day / msg.value)*blocksPer30Day; */
+            if (rentpaidUntill[tennant] == 0) {
+                rentpaidUntill[tennant] = block.number + _additionalblocks;
+            }
+            else {
+                rentpaidUntill[tennant] = rentpaidUntill[tennant] +_additionalblocks;
+            }
+            }
+
+
+
+            receive () external payable {
+                (msg.sender).transfer(msg.value);
+
+
+
+
         }
 
 
